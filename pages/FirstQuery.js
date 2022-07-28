b64ebc12011e00d270af6790f3017be8e2b09cec
sfetcu20 <sfetcualexandra19@stud.ase.ro>
2022-07-28 20:13:00 +0300

    reactQuery +btn fetch

--- pages/FirstQuery.js
+++ pages/FirstQuery.js
@@ -4,9 +4,12 @@ const axios = require('axios').default;
 
 function FirstQuery() {
   const [image,setImage] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   const fetchPlanet = async () => {
+    setIsLoading(true)
     const res= await axios.get('https://dog.ceo/api/breeds/image/random')
     setImage(res.data.message);
+    setIsLoading(false)
   }
   useEffect(()=>{
     fetchPlanet();
@@ -14,6 +17,7 @@ function FirstQuery() {
   return (
     <div>first-query
       <div>
+        <p>{isLoading && "is loading"}</p>
         <img src={image} alt="monster"/>
       </div>
     </div>
--- pages/PageTwo.js
+++ pages/PageTwo.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { useState } from 'react';
 import {useQuery} from 'react-query'
 const axios = require('axios').default;
+
 function PageTwo() {
-    const [image,setImage]=useState({})
-    const getDog=async ()=>{
-        const {data, status} = await axios.get('https://dog.ceo/api/breeds/image/random');
-        setImage(data.message)
+    const handleClick = ()=>{
+        refetch();
     }
-    const {data,status} = useQuery("dogs",getDog);
+    const getDog=async ()=>{
+        const res = await axios.get('https://dog.ceo/api/breeds/image/random');
+        console.log(res)
+        if (res.status===200) return res.data.message
+        
+    }
+    const {data,isLoading,refetch} = useQuery("dogs",getDog, {refetchOnWindowFocus: false,
+                        enabled: false});
     return (
-    <div>PageTwo  
-        <img src={image}/>
+    <div>PageTwo 
+        <p>{isLoading && "is loading"}</p> 
+        <img src={data}/> 
+        <button onClick={handleClick} value="Get new picture">Get new picture</button>
     </div>
   )
 }
